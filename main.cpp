747c2a08700e736fc6e008f34118c8ff73a156ae
S5243141 <99831210+S5243141@users.noreply.github.com>
2022-05-06 15:28:54 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
+int main() {
+  char b;
+  cin >> b;
+  int a = (int)b;
+  bool x = carattere(a);
+  
+  if (x==1) {
+    if ((a >= 65) && (a <= 90)) {
+      a = a+32;
+      cout << (char)a <<endl;
+    } else if ((a >= 97) && (a <= 122)) {
+      a = a-32;
+      cout << (char)a <<endl;
+    }
+  } else {
+    cout << "errore" <<endl;
+  }
   return 0;
 }
